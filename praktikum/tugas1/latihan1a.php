8ce0e50d35cbaa12c8e9845a2eb7b310c72d6489
kelvin-203040062 <75973924+kelvin-203040062@users.noreply.github.com>
2021-03-04 23:12:42 +0700

    tugas praktikum selesai

---
+++ praktikum/tugas1/latihan1a.php
@@ -0,0 +1,16 @@
+<!-- 
+Nama : kelvin ardiansah
+NRP : 203040062
+Shift Praktikum : Jumat 10.00 
+-->
+
+
+ <?php
+
+for( $i = 1; $i <= 3; $i++ ) {
+    for( $j = 1; $j <= 3; $j++ ) {
+        echo "Ini perulangan ke ($i,$j) <br>";
+    }
+}
+
+?>
