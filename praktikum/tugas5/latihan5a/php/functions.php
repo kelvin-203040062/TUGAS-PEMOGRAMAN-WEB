7c39bad638c6fbfd3c4c36703ac5bcbd00c24ac0
kelvin-203040062 <75973924+kelvin-203040062@users.noreply.github.com>
2021-04-15 03:07:51 +0700

    tugas

---
+++ praktikum/tugas5/latihan5a/php/functions.php
@@ -0,0 +1,27 @@
+<!-- 
+    NAMA : kelvin ardiansah
+    NPM : 203040062
+    Kelas : B
+    MataKuliah : Praktikum Pemrograman Web
+    Shift : Jumat, 10.00 WIB
+ -->
+
+<?php
+function koneksi()
+{
+    $conn = mysqli_connect("localhost", "root", "");
+    mysqli_select_db($conn, "pw_tubes_203040075");
+
+    return $conn;
+}
+
+function query ($sql)
+{
+    $conn = koneksi();
+    $result = mysqli_query($conn, "$sql");
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
